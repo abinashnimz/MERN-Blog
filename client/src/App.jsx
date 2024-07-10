7dcde85fd75eac01a1929120ed4289ff5c10702d
abinashnimz <abinash443steam@gmail.com>
2024-07-10 14:07:44 +0530

    Added all pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,24 @@
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Project } from "./pages/Project";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Dashboard } from "./pages/Dashboard";
+
+
+
 export const App = ()=>{
-  return <h1 className="text-3xl">App</h1>
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
