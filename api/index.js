9664667f77829343a118d6057197ba7fa70feb7b
abinashnimz <abinash443steam@gmail.com>
2024-07-11 12:22:38 +0530

    Added mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+
+
 const app = express();
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("database connecttion successful"))
+.catch((err)=>console.log(err));
+
+
 
 
 app.listen(3000, ()=>{
