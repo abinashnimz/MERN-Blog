445f57c0e2707fac0dfea19b9ee0b5744e98d3a9
abinashnimz <abinash443steam@gmail.com>
2024-07-15 13:24:16 +0530

    Added profile section in the header navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-export const Header = ()=>{
+export const Header = () => {
     const path = useLocation().pathname;
-    return(
+    const { currentUser } = useSelector(state => state.user);
+    console.log(currentUser);
+    return (
         <Navbar className="border-b-2">
             <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
                 <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Nim'z</span>
@@ -21,12 +24,40 @@ export const Header = ()=>{
                 <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
                     <FaMoon />
                 </Button>
-                <Link to="/sign-in">
-                    <Button gradientDuoTone="purpleToBlue" outline>
-                        Sign In
-                    </Button>
-                </Link>
-                <Navbar.Toggle/>
+                {
+                    (currentUser) ? (
+                        <Dropdown
+                        arrowIcon={false}
+                        inline
+                        label={
+                            <Avatar
+                                alt="user"
+                                rounded
+                                img={currentUser.userdata.profilePic}
+                            />
+                        }
+                        >
+                            <Dropdown.Header>
+                                <span className="block text-sm">@{currentUser.userdata.username}</span>
+                                <span className="block text-sm font-medium truncate">{currentUser.userdata.email}</span>
+                            </Dropdown.Header>
+                            <Link to={"/dashboard?tab=profile"}>
+                            <Dropdown.Item>Profile</Dropdown.Item>
+                            </Link>
+                            <Dropdown.Divider/>
+                            <Dropdown.Item>Sign out</Dropdown.Item>
+                        </Dropdown>
+                    ) :
+                        (
+                            <Link to="/sign-in">
+                                <Button gradientDuoTone="purpleToBlue" outline>
+                                    Sign In
+                                </Button>
+                            </Link>
+                        )
+                }
+
+                <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
                 <Navbar.Link active={path === "/"} as={"div"}>
