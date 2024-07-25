aeb51a95d7efaf6e7bd42ec5fc1420fd125365de
abinashnimz <abinash443steam@gmail.com>
2024-07-25 22:20:31 +0530

    Added create comment api logic

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
 import errorRouter from "./routes/error.route.js";
 import { errorMiddleware } from "./middleware/error.middleware.js";
 import {connectDB} from "./utils/db.js";
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
 app.use("*", errorRouter);
 
 app.use(errorMiddleware);
