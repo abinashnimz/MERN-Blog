41a97086070660901afaf6de0e42bee34115a47d
abinashnimz <abinash443steam@gmail.com>
2024-07-26 21:56:25 +0530

    Added edit comment api logic

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -34,7 +34,7 @@ export const likeComment = async (req, res, next)=>{
     try{
         const comment = await Comment.findById(req.params.commentId);
         if(!comment){
-            return nexy(errorHandler(404, "comment not found"));
+            return next(errorHandler(404, "comment not found"));
         }
         const userIndex = comment.likes.indexOf(req.user.userId);
         console.log("userIndex", userIndex);
@@ -47,6 +47,24 @@ export const likeComment = async (req, res, next)=>{
         }
         await comment.save();
         res.status(201).json(comment);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const editComment = async (req, res, next)=>{
+    try{
+        const comment = await Comment.findById(req.params.commentId);
+        if(!comment){
+            return next(errorHandler(404, "Comment not found"));
+        }
+        if((comment.userId !== req.user.userId) && !req.user.isAdmin){
+            return next(errorHandler(404, "You are not allowed to edit this comment"));
+        }
+        const editedComment = await Comment.findByIdAndUpdate(req.params.commentId, {
+            content:req.body.content,
+        }, {new:true});
+        res.status(201).json(editedComment);
     }catch(err){
         next(err);
     }
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyUser.middleware.js";
-import { createComment, getComments, likeComment } from "../controllers/comment.controller.js";
+import { createComment, getComments, likeComment, editComment } from "../controllers/comment.controller.js";
 
 
 const route = express.Router();
@@ -8,5 +8,6 @@ const route = express.Router();
 route.post("/create", verifyToken, createComment);
 route.get("/getcomments/:postId", getComments);
 route.put("/likecomment/:commentId", verifyToken, likeComment);
+route.put("/editcomment/:commentId", verifyToken, editComment);
 
 export default route;
