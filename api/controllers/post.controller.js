3a3ebcc61cfe9c493e0159cc9ad7982a86c2521e
abinashnimz <abinash443steam@gmail.com>
2024-07-30 17:31:25 +0530

    minor changes in getposts

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -40,6 +40,7 @@ export const getposts = async (req, res, next)=>{
                 $or:[
                     {title: {$regex: req.query.searchTerm, $options:"i"}},
                     {content: {$regex: req.query.searchTerm, $options:"i"}},
+                    {category: {$regex: req.query.searchTerm, $options:"i"}},
                 ],
             }),
         }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
