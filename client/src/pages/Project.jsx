6f57bbb6e69632995359b24af0adf468e352fdc7
abinashnimz <abinash443steam@gmail.com>
2024-07-30 22:38:40 +0530

    added about page and project page UI

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,5 +1,11 @@
+import { CallToAction } from "../components/CallToAction"
+
 export const Project = ()=>{
     return(
-        <h1>Project</h1>
+        <div className="flex flex-col items-center justify-center gap-6 min-h-screen max-w-2xl mx-auto p-3">
+            <h1 className="text-3xl font-semibold text-center">Pojects</h1>
+            <p className="text-md text-gray-500">Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+            <CallToAction/>
+        </div>
     )
 }
